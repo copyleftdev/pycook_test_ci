7f11f2796cd3cc314b7158fc6a3fb2f7763d76c1
Don Johnson <dj@codetestcode.io>
2014-12-02 08:28:14 -0800

    Added two tests test_unpack_seq_to_sperate_vars, and test_keeping_last_n_items

--- app/codetestcodeutils.py
+++ app/codetestcodeutils.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from collections import deque
 
 class codetestcodeutils(object):
 
   def unpack_seq_to_seperate_vars(self):
-    """
+    '''
           1.1. Unpacking a Sequence into Separate Variables
           Problem
           -----------------------
@@ -14,7 +15,7 @@ class codetestcodeutils(object):
           -----------------------
           Any sequence (or iterable) can be unpacked into variables using a simple assignment operation.
           The only requirement is that the number of variables and structure match the sequence. For example:
-    """
+    '''
 
     a = (1, 2)
     data_flat = [1, 2, 3]
@@ -24,3 +25,12 @@ class codetestcodeutils(object):
     one, two, three = data_flat
     name, date_of_birth, location = data_nested
     w,o,r,l,d = hello
+
+    return(x,y,one,two,three, name, date_of_birth,location,w,o,r,l,d)
+
+  def keep_last_n_items(self, lines, patterns,history=5):
+    previous_lines = deque(maxlen=history)
+    for line in  lines:
+      if patterns in line:
+        yield line, previous_lines
+      previous_lines.append(line)
---
+++ tests/codetestcodeutils_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import unittest
+from app.codetestcodeutils import codetestcodeutils
+
+
+
+class TestCodeTestCodeSuite(unittest.TestCase):
+
+   def test_unpack_seq_to_sperate_vars(self):
+    test_vector = codetestcodeutils()
+    self.assertEqual(test_vector.unpack_seq_to_seperate_vars(),(1, 2, 1, 2, 3, "Don Johnson", (6, 23, 1795),"Pasadena", "h", "e", "l", "l", "o"))
+
+   def test_keeping_last_n_items(self):
+    test_vector = codetestcodeutils()
+    with open('tests/data/sample.txt') as f:
+      for line, prevlines, in  test_vector.keep_last_n_items(f,'test',5):
+        for pline in prevlines:
+          self.assertEqual(12,len(pline))
+
+
+
+if __name__ == '__main__':
+  unittest.main()
