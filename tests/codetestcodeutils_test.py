a6d09f81d7e2b3db6eef4d809b6bfa1cee2605ba
Don Johnson <dj@codetestcode.io>
2014-12-04 08:36:30 -0800

    Rerfactor test_vector variable to tv to save of space

--- tests/codetestcodeutils_test.py
+++ tests/codetestcodeutils_test.py
@@ -8,18 +8,18 @@ from app.codetestcodeutils import codetestcodeutils
 class TestCodeTestSuite(unittest.TestCase):
 
   def test_unpack_seq_to_sperate_vars(self):
-    test_vector = codetestcodeutils()
-    self.assertEqual(test_vector.unpack_seq_to_seperate_vars(),(1, 2, 1, 2, 3, "Don Johnson", (6, 23, 1795),"Pasadena", "h", "e", "l", "l", "o"))
+    tv = codetestcodeutils()
+    self.assertEqual(tv.unpack_seq_to_seperate_vars(),(1, 2, 1, 2, 3, "Don Johnson", (6, 23, 1795),"Pasadena", "h", "e", "l", "l", "o"))
 
   def test_keeping_last_n_items(self):
-    test_vector = codetestcodeutils()
+    tv = codetestcodeutils()
     with open('tests/data/sample.txt') as f:
-      for line, prevlines, in  test_vector.keep_last_n_items(f,'test',5):
+      for line, prevlines, in  tv.keep_last_n_items(f,'test',5):
         for pline in prevlines:
           self.assertEqual(12,len(pline))
 
   def test_find_smallest_n_items(self):
-    test_vector = codetestcodeutils()
+    tv = codetestcodeutils()
     portfolio = [
       {'name':'IBM', 'shares':100, 'price':91.1},
       {'name':'APPL', 'shares':50, 'price':543.22},
@@ -29,10 +29,10 @@ class TestCodeTestSuite(unittest.TestCase):
       {'name':'ACME', 'shares':75, 'price':115.65}
     ]
 
-    self.assertEqual(test_vector.find_smallest_n_items(3,portfolio,"price"), [{'price': 16.35, 'name': 'IBM', 'shares': 45}, {'price': 21.09, 'name': 'IBM', 'shares': 200}, {'price': 31.75, 'name': 'IBM', 'shares': 35}])
+    self.assertEqual(tv.find_smallest_n_items(3,portfolio,"price"), [{'price': 16.35, 'name': 'IBM', 'shares': 45}, {'price': 21.09, 'name': 'IBM', 'shares': 200}, {'price': 31.75, 'name': 'IBM', 'shares': 35}])
 
   def test_find_largest_n_items(self):
-     test_vector = codetestcodeutils()
+     tv = codetestcodeutils()
      portfolio = [
       {'name':'IBM', 'shares':100, 'price':91.1},
       {'name':'APPL', 'shares':50, 'price':543.22},
@@ -41,12 +41,12 @@ class TestCodeTestSuite(unittest.TestCase):
       {'name':'IBM', 'shares':45, 'price':16.35},
       {'name':'ACME', 'shares':75, 'price':115.65}
     ]
-     self.assertEqual(test_vector.find_largest_n_items(3,portfolio,"price"),[{'price': 543.22, 'name': 'APPL', 'shares': 50}, {'price': 115.65, 'name': 'ACME', 'shares': 75}, {'price': 91.1, 'name': 'IBM', 'shares': 100}])
+     self.assertEqual(tv.find_largest_n_items(3,portfolio,"price"),[{'price': 543.22, 'name': 'APPL', 'shares': 50}, {'price': 115.65, 'name': 'ACME', 'shares': 75}, {'price': 91.1, 'name': 'IBM', 'shares': 100}])
   def test_map_to_multiple_values(self):
-    test_vector = codetestcodeutils()
+    tv = codetestcodeutils()
     test_list = list(range(0,20))
     assert_vector =  {'vector_a': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]}
-    self.assertEqual(test_vector.map_keys_to_mutiple_values('temp','vector_a',test_list),assert_vector)
+    self.assertEqual(tv.map_keys_to_mutiple_values('temp','vector_a',test_list),assert_vector)
 
 
 
